72c0a02515eb6ce3cb143ce5443574dc8ca52568
Pranes <pranesracer@gmail.com>
2023-10-13 11:08:17 -0400

    changedvariables

--- 4-names.js
+++ 4-names.js
@@ -3,5 +3,6 @@ const secret = "SUPER SECRET"
 //share
 const john = 'John'
 const peter = 'Peter'
+const gay = 'koushalgay'
 
-module.exports = {john,peter}
+module.exports = {john,peter,koushalgay}
